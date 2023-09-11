c2b2188d9933f18e8a98a40c03afa763f8d8ec34
Beonny <bonifacekipkirui98@gmail.com>
2023-09-11 12:34:17 -0700

    print all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description: print all the numbers of base 16
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+char hex_digits[] = "0123456789abcdef";
+for (int i = 0; i < 16; i++)
+{
+	putchar(hex_digits[i]);
+}
+putchar('\n');
+return (0);
+}
+

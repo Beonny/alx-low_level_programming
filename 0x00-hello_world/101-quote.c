fcb2c5f08fd54297ab4cfb9b049cfea2843d5103
Beonny <bonifacekipkirui98@gmail.com>
2023-09-11 08:58:04 -0700

    program that print using fput

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main- Entry point
  * Description: programs that prints without using printf/puts
- * return 1
+ * Return: 1 (success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
